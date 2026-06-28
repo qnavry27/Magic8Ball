ef0c48b6ecb589b233ff8c10919681b18c582209
qnvary27 <daniel.machtolf@proton.me>
2026-06-28 11:49:42 +0200

    Finished project.

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 Magic 8 Ball in Rust
 
 # Todo
-- [ ] Implement the 20 possible answers the Magic-8-Ball can provide
-- [ ] Ask the player if they want to play again
-- [ ] User Input + Validation / Error Handling
--
+- [x] Implement the 20 possible answers the Magic-8-Ball can provide
+- [x] Randomly choose one of the possible answers
+- [x] Ask the player if they want to play again (Game Loop & Game State)
+- [x] User Input + Validation / Error Handling
+
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
- /// Magic 8 Ball.
- /// The player has the possiblity to provide any question and in return
- /// receives an randomly chosen answer (yes or no) from 20 possible choices.
+ //! Magic 8 Ball.
+ //! 
+ //! The player has the possiblity to provide any question and in return
+ //! receives an randomly chosen answer (yes or no) from 20 possible choices.
  
  use rand::seq::SliceRandom;
+ use std::{io, thread, time};
 
+ /// Represents the 20 answers a Magic 8-Ball can give.
  #[derive(Debug, Clone, Copy)]
  enum Answers {
     ItIsCetain,
@@ -29,6 +32,7 @@
  }
 
  impl Answers {
+    /// Returns the human-readable string representation of the 8-Ball answer.
     pub fn as_string(&self) -> &'static str {
         match self {
             Self::ItIsCetain => "It is certain.",
@@ -54,41 +58,104 @@
             
         }
     }
+
+    const ALL: [Answers; 20] = [
+        Self::ItIsCetain,
+        Self::ItIsDecidedlySo,
+        Self::WithoutADoubt,
+        Self::YesDefinitely,
+        Self::YouMayRelyOnIt,
+        Self::AsIseeItYes,
+        Self::MostLikely,
+        Self::OutlookGood,
+        Self::Yes,
+        Self::SignsPointToYes,
+        Self::ReplyHazyTryAgain,
+        Self::AskAgainLater,
+        Self::BetterNotTellYouNow,
+        Self::CannotPredcitNow,
+        Self::ConcentrateAndAskAgain,
+        Self::DontCountOnIt,
+        Self::MyReplyIsNo,
+        Self::MySourcesSayNo,
+        Self::OutlookNotSoGood,
+        Self::VeryDoubtful,
+    ];
+
+    /// Simulates shaking the Magic 8-Ball by randomly selecting one of the 20 answers.
     pub fn shake() -> Self{
         // Randomly choose one of the replies.
         let mut rng = rand::thread_rng();
-        let answers: [Answers; 20] = [
-            Self::ItIsCetain,
-            Self::ItIsDecidedlySo,
-            Self::WithoutADoubt,
-            Self::YesDefinitely,
-            Self::YouMayRelyOnIt,
-            Self::AsIseeItYes,
-            Self::MostLikely,
-            Self::OutlookGood,
-            Self::Yes,
-            Self::SignsPointToYes,
-            Self::ReplyHazyTryAgain,
-            Self::AskAgainLater,
-            Self::BetterNotTellYouNow,
-            Self::CannotPredcitNow,
-            Self::ConcentrateAndAskAgain,
-            Self::DontCountOnIt,
-            Self::MyReplyIsNo,
-            Self::MySourcesSayNo,
-            Self::OutlookNotSoGood,
-            Self::VeryDoubtful,
-        ];
-        *answers.choose(&mut rng).unwrap()
+        *Self::ALL.choose(&mut rng).unwrap()
     }
 }
+
+/// Controls the flow of the main game loop.
+#[derive(Debug, PartialEq)]
+enum GameState{
+    Continue,
+    Quit,
+}
+impl GameState{
+    /// Prompts the user to decide whether to play another round or quit.
+    pub fn get_rematch_status() -> GameState {
+        loop{
+            println!("Do you want to play another round (y/n):");
+            let mut input = String::new();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read line");
+
+            match input.trim().to_lowercase().as_str() {
+                "y" | "yes" => return GameState::Continue,
+                "n" | "no" | "quit" => return GameState::Quit,
+                _ => println!("Please type 'y' for yes or 'n' for no.\n"),
+            }
+        }
+    }
+}
+
+///Reads a question from the player via standard input.
+/// 
+/// This function enforces that the input must contain a question mark.
+/// If it doesn't, it displays an error and promts the user again.
+fn get_player_input() -> String {
+    loop {
+        println!("Ask a yes/no question");
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        if input.contains("?"){
+            return input;
+        } else {
+            println!("Input was not a question, please provide a ?");
+            continue
+        }
+    }   
+}
  
+ /// The main entry point of the game. 
+ /// Manages the primary game loop, time delays, and matching states.
 fn main() {
-    // ADD INPUT
-    println!("Your question is");
-    println!("Shaking the Magic 8 Ball...\n");
 
-    let answer = Answers::shake();
+    println!("Welcome to the Magic-8-Ball!\n");
+    loop {
+        let question = get_player_input();
 
-    println!("Answer: {}", answer.as_string());
+        println!("Your question is: {question}\n");
+        println!("Shaking the Magic 8 Ball...\n");
+
+        thread::sleep(time::Duration::from_millis(1500));
+
+        let answer = Answers::shake();
+        println!("Answer: {}", answer.as_string());
+
+        if GameState::get_rematch_status() == GameState::Quit {
+            println!("Bye");
+            break;
+        }
+        println!("")
+    }
 }
